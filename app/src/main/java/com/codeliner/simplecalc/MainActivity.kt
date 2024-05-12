f5c170ac18388288d7a8d6e1fa4595e2c3652ab0
Nikita Novgorodtsev <novgorodcevn5@gmail.com>
2024-05-12 22:56:23 +0300

    Add files

--- app/src/main/java/com/codeliner/simplecalc/MainActivity.kt
+++ app/src/main/java/com/codeliner/simplecalc/MainActivity.kt
@@ -5,18 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.WindowManager
 import android.widget.Button
+import android.widget.GridLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.children
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     lateinit var viewModel: MainActivityViewModel
-    private lateinit var buttonIncrease: Button
-    private lateinit var counterTv: TextView
-    private lateinit var buttonClear: Button
-    private lateinit var buttonDecrease: Button
+    
+    private lateinit var resultTv: TextView
+    private lateinit var formulaTv: TextView
+
+
+     lateinit var layotMain: GridLayout
+
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
@@ -36,24 +43,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
-        buttonIncrease = findViewById(R.id.button_increase)
-        counterTv = findViewById(R.id.counter_tv)
-        buttonClear = findViewById(R.id.button_clear)
-        buttonDecrease = findViewById(R.id.button_decrease)
 
-        viewModel.getCounterValueLiveData().observe(this, Observer {
-            counterTv.text = it.toString()
+        resultTv = findViewById(R.id.tv_result)
+        formulaTv = findViewById(R.id.tv_formula)
+        layotMain = findViewById(R.id.layout_main)
+
+
+        viewModel.getResultValueLiveData().observe(this, Observer {
+            resultTv.text = it.toString()
         })
-        buttonIncrease.setOnClickListener {
-            viewModel.onButtonClick(R.id.button_increase)
+        viewModel.getFormulaValueLiveData().observe(this, Observer{
+            formulaTv.text = it.toString()
+        })
+
+        layotMain.children.filterIsInstance<Button>().forEach {button ->
+
+
+            button.setOnClickListener {
+                viewModel.buttont2 = button
+                viewModel.onButtonClick(R.id.layout_main)
+
+            }
         }
-        buttonDecrease.setOnClickListener {
-            viewModel.onButtonClick(R.id.button_decrease)
-        }
-        buttonClear.setOnClickListener {
-            viewModel.onButtonClick(R.id.button_clear)
-        }
+
+
+
     }
 }
--- app/src/main/java/com/codeliner/simplecalc/MainActivityViewModel.kt
+++ app/src/main/java/com/codeliner/simplecalc/MainActivityViewModel.kt
@@ -1,38 +1,102 @@
 package com.codeliner.simplecalc
 
+import android.widget.Button
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
-    private var counterValue = 0
-    private val counterValueLiveData = MutableLiveData<Int>()
 
-    fun getCounterValueLiveData(): LiveData<Int> = counterValueLiveData
+    private val resultValueLiveData = MutableLiveData<String>()
+    private val formulaValueLiveData = MutableLiveData<String>()
 
+
+    lateinit var buttont2: Button
+    var currentNumber = ""
+    var currentOperator = ""
+    var result = ""
+    var firstNumber = ""
+
+    fun getResultValueLiveData(): LiveData<String> = resultValueLiveData
+    fun getFormulaValueLiveData(): LiveData<String> = formulaValueLiveData
     fun onButtonClick(buttonId: Int) {
         when (buttonId) {
-            R.id.button_increase -> increaseCount()
-            R.id.button_decrease -> decreaseCount()
-            R.id.button_clear -> clearCount()
+            R.id.layout_main -> buttonValue()
         }
     }
-    private fun increaseCount() {
 
-        ++counterValue
-        if (counterValue > 10) counterValue = 10
-        counterValueLiveData.value = counterValue
-    }
-    private fun decreaseCount() {
 
-        --counterValue
-        if (counterValue < 0) counterValue = 0
-        counterValueLiveData.value = counterValue
-    }
-    private fun clearCount() {
+    fun buttonValue() {
 
-        counterValue = 0
-        counterValueLiveData.value = counterValue
+        val buttonText = buttont2.text.toString()
+
+        when {
+            buttonText.matches(Regex("[0-9]")) -> {
+
+                if (currentOperator.isEmpty()) {
+                    firstNumber += buttonText
+                    resultValueLiveData.value = firstNumber
+                } else {
+                    currentNumber += buttonText
+
+                    resultValueLiveData.value = currentNumber
+                }
+            }
+            buttonText.matches(Regex("[+\\-*/]")) -> {
+                currentNumber = ""
+                if (resultValueLiveData.value.toString().isNotEmpty()) {
+                    currentOperator = buttonText
+                    resultValueLiveData.value = "0"
+                }
+            }
+            buttonText == "=" -> {
+                if (currentNumber.isNotEmpty() && currentOperator.isNotEmpty()) {
+                    formulaValueLiveData.value = "$firstNumber$currentOperator$currentNumber"
+                    result = evaluaterExpression(firstNumber, currentNumber, currentOperator)
+                    firstNumber = result
+                    resultValueLiveData.value = result
+                }
+            }
+            buttonText == "." -> {
+                if (currentOperator.isEmpty()) {
+                    if (!firstNumber.contains(".")) {
+                        if (firstNumber.isEmpty()) firstNumber += "0$buttonText"
+                        else firstNumber += buttonText
+                        resultValueLiveData.value = firstNumber
+                    }
+                } else {
+                    if (!currentNumber.contains(".")) {
+                        if (currentNumber.isEmpty()) currentNumber += "0$buttonText"
+                        else currentNumber += buttonText
+                        resultValueLiveData.value = currentNumber
+                    }
+                }
+            }
+            buttonText == "c" -> {
+                currentNumber = ""
+                firstNumber = ""
+                currentOperator = ""
+                resultValueLiveData.value = "0"
+                formulaValueLiveData.value = ""
+            }
+        }
+
     }
+
+    private fun evaluaterExpression(
+        firstNumber: String,
+        secondNumber: String,
+        operator: String
+    ): String {
+        val num1 = firstNumber.toDouble()
+        val num2 = secondNumber.toDouble()
+        return when (operator) {
+            "+" -> (num1 + num2).toString()
+            "-" -> (num1 - num2).toString()
+            "*" -> (num1 * num2).toString()
+            "/" -> (num1 / num2).toString()
+            else -> ""
+        }
+    }
+
 }
-
